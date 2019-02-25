3c218871bc6d6133a9ba7f3b1ceddb5427c5d09f
Shahfazlollahi <n_shahfazlollahi@tss-hr420-dm38.fc.ca>
2019-02-25 15:44:06 -0500

    added other tracks

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 	let aud = document.querySelector('audio'),
 		play = document.querySelector('#play'),
 		pause = document.querySelector('#pause'),
-		rewind = document.querySelector('#rewind');
+		rewind = document.querySelector('#rewind'),
+		tracks = document.querySelectorAll('.trackholder');
 
 		//write function for audio element
 		function playAudio() {
@@ -18,9 +19,20 @@
 			aud.currentTime = 0;
 		}
 
+		function swapSource() {
+			let currentTrack = this.dataset.currenttrack;
+
+			aud.src = `audio/${currentT
+				rack}`;
+			aud.load();
+			aud.play();
+		}
+
 		//add event handling
 		
 		play.addEventListener("click", playAudio);
 		pause.addEventListener("click", pauseAudio);
 		rewind.addEventListener("click", rewindAudio);
+
+		tracks.forEach(track => track.addEventListener("click", swapSource));
 })();
